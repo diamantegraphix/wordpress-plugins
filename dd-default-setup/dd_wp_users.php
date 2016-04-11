2a8074d503d6f7a94540ef9185ac0a55a30174d1
Bethany Gresser <bethany@diamantegraphix.com>
2016-04-11 14:03:10 -0400

    current users

---
+++ dd-default-setup/dd_wp_users.php
@@ -0,0 +1,54 @@
+<?php 
+$dd_wp_users = array (
+	array (
+	    'user_login' => 'BLDD1102',
+	    'user_pass'  => 'D1amante!0457',
+	    'user_email' => 'bethany@diamantedesign.solutions',
+	    'first_name' => 'Bethany',
+	    'last_name' => 'Gresser',
+	    'display_name' => 'Bethany',
+	    'nickname' => 'Bethany',
+	    'role' => 'administrator'
+	),
+	array (
+	    'user_login' => 'TLDD1103',
+	    'user_pass'  => 'D1amante!0256',
+	    'user_email' => 'tanya@diamantedesign.solutions',
+	    'first_name' => 'Tanya',
+	    'last_name' => 'Amaya',
+	    'display_name' => 'Tanya',
+	    'nickname' => 'Tanya',
+	    'role' => 'administrator'
+	),
+	array (
+	    'user_login' => 'ESDD2101',
+	    'user_pass'  => 'D1am0nd',
+	    'user_email' => 'erik@genoasboy.com',
+	    'first_name' => 'Erik',
+	    'last_name' => 'Sams',
+	    'display_name' => 'Erik',
+	    'nickname' => 'Erik',
+	    'role' => 'administrator'
+	),
+	array (
+	    'user_login' => 'REDD5101',
+	    'user_pass'  => 'D1am0nd',
+	    'user_email' => 'randall@diamantedesign.solutions',
+	    'first_name' => 'Randall',
+	    'last_name' => 'Evans',
+	    'display_name' => 'Randall',
+	    'nickname' => 'Randall',
+	    'role' => 'administrator'
+	),
+	array (
+	    'user_login' => 'EBDD4101',
+	    'user_pass'  => 'D1am0nd',
+	    'user_email' => 'eve@diamantedesign.solutions',
+	    'first_name' => 'Eve',
+	    'last_name' => 'Brettell',
+	    'display_name' => 'Eve',
+	    'nickname' => 'Eve',
+	    'role' => 'administrator'
+	), 
+);
+?>
