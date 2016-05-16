820f12fe7be4a27f77d3fcd5164651a12247f2b5
Bethany Gresser <bethany@diamantegraphix.com>
2016-05-16 15:55:43 -0400

    Added plugins, rename to dd-wp-defaults.php
    Added delete plugins array

--- dd-default-setup/dd-wp-defaults.php
+++ dd-default-setup/dd-wp-defaults.php
@@ -1,5 +1,5 @@
 <?php 
-$dd_wp_users = array (
+$dd_users = array (
 	array (
 	    'user_login' => 'BLDD1102',
 	    'user_pass'  => 'D1amante!0457',
@@ -8,6 +8,7 @@ $dd_wp_users = array (
 	    'last_name' => 'Gresser',
 	    'display_name' => 'Bethany',
 	    'nickname' => 'Bethany',
+	    'user_nicename' => 'bethany',
 	    'role' => 'administrator'
 	),
 	array (
@@ -18,6 +19,7 @@ $dd_wp_users = array (
 	    'last_name' => 'Amaya',
 	    'display_name' => 'Tanya',
 	    'nickname' => 'Tanya',
+	    'user_nicename' => 'tanya',
 	    'role' => 'administrator'
 	),
 	array (
@@ -28,6 +30,7 @@ $dd_wp_users = array (
 	    'last_name' => 'Sams',
 	    'display_name' => 'Erik',
 	    'nickname' => 'Erik',
+	    'user_nicename' => 'erik',
 	    'role' => 'administrator'
 	),
 	array (
@@ -38,6 +41,7 @@ $dd_wp_users = array (
 	    'last_name' => 'Evans',
 	    'display_name' => 'Randall',
 	    'nickname' => 'Randall',
+	    'user_nicename' => 'randall',
 	    'role' => 'administrator'
 	),
 	array (
@@ -48,7 +52,13 @@ $dd_wp_users = array (
 	    'last_name' => 'Brettell',
 	    'display_name' => 'Eve',
 	    'nickname' => 'Eve',
+	    'user_nicename' => 'eve',
 	    'role' => 'administrator'
-	), 
+	)
+);
+
+$dd_delete_plugins = array (
+    'akismet/akismet.php',
+    'jetpack/jetpack.php'
 );
 ?>
