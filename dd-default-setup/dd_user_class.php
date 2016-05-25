dcc522559463dd07660363f5b44a2042c8cf78de
Bethany Gresser <bethany@diamantegraphix.com>
2016-05-25 16:13:17 -0400

    in progress - writing classes for data sets
    started writing class for data sets in settings file, not finished

--- dd-default-setup/dd_user_class.php
+++ dd-default-setup/dd_user_class.php
@@ -1,3 +1,46 @@
+class dd_settings {
+
+	private $add_users;
+	private $delete_plugins;
+	private $add_plugins;
+
+	public function __construct($file) {
+		if (file_exists ($file)) {
+			require_once ($file);
+			$user_set = new dd_user_set($dd_users);
+
+		} else {
+			// FAIL
+		}
+	}
+
+	public function __destruct() {
+		return false;
+	}
+
+}
+
+class dd_user_set {
+	private 
+
+	public function __construct($users) {
+		foreach ($users as $user) {
+						
+
+	
+		}
+
+
+}
+
+
+
+
+
+
+
+
+
 class dd_user {
 
 	public $userdata;
@@ -72,21 +115,26 @@ class dd_user {
 		return false;
 	}
 
-	// Returns userdata in an array
-	protected function get_userdata () {
+	// Returns user data in an array
+	protected function get_userdata() {
+
+		// Create user data array
 		$userdata = array ();
 
+		// Add ID, or empty ID if none set
 		if (!empty($this->ID)) {
 			$userdata['ID'] = $this->ID;
 		} else {
 			$userdata['ID'] = '';
 		}
-
+		
+		// Add required data
 		$userdata['user_login'] = $this->user_login;
 		$userdata['user_email'] = $this->user_email;
 		$userdata['user_pass'] = $this->user_pass;
 		$userdata['role'] = $this->role;
 
+		// Add conditional data if exists
 		if (!empty($this->first_name)) {
 			$userdata['first_name'] = $this->first_name;
 		}
@@ -103,17 +151,31 @@ class dd_user {
 			$userdata['display_name'] = $this->display_name;
 		}
 
+		// Return user data array
 		return $userdata;
 	}
 
 	// Check if user can be added or updated
 	private function dd_can_add() {
 		if (username_exists($this->user_login])) {
-			$this->ID = username_exists($this->user_login);
+			
+			// Username exists, user can be updated not added
 			$add = 'update';
+
+			// Set ID to existing user ID
+			$this->ID = username_exists($this->user_login);
+
 		} else if (email_exists($userdata->user_email)) {
+
+			// Email exists and username does not, user cannot be added
 			$add = false;
+			
+			// Set ID to false
+			$this->ID = username_exists($this->user_login);
+
 		} else {
+
+			// Username and email do not exist, user can be added
 			$add = 'add';
 		}
 
@@ -121,30 +183,49 @@ class dd_user {
 	}
 
 	// Add user, update user, or return false.
-	public function dd_add_user () {
+	public function dd_add_user() {
+
+		// Determine if username will be added, updated, or no action
 		$add = dd_can_add();
+
 		if ($add == 'add') {
+
+			// User to be added
 			$user_id = wp_insert_user ($this->get_userdata);
+
+			// Check for error adding user
 			if (!is_wp_error($user_id)) {
 				$this->ID = $user_id;
 			} else {
 				$this->ID = false;
 			}
+
 		} else if ($add == 'update') {
+
+			// User to be updated
 			$user_id = wp_update_user ($this->get_userdata);
+
+			// Check for error updating user
 			if (!is_wp_error($user_id)) {
 				$this->ID = $user_id;
 			} else {
 				$this->ID = false;
 			}
+
 		} else {
+
+			// No action with user
 			$this->ID = false;
+
 		}
+
+		// Return user ID on successful add or update, return false on error
 		return $this->ID;
 	}
 
 	public dd_delete_user ($user) {
 		//TODO
 	}
+
 
 }
